e1156bb149b630a9a949f0304c80fd29b06e0199
egekaplan <egekaplann@gmail.com>
2024-02-08 10:10:28 +0300

    black formatted

--- tests/test_working_days.py
+++ tests/test_working_days.py
@@ -4,12 +4,14 @@ import pytest
 from src.working_days_project.working_days_tr import count_working_days
 from loguru import logger  # Import Loguru's logger
 
+
 @pytest.fixture
 def holidays():
     cal = Turkey()
     this_year = datetime.date.today().year
     holidays_list = cal.holidays(this_year)
-    return [date.strftime('%Y-%m-%d') for date, _ in holidays_list]
+    return [date.strftime("%Y-%m-%d") for date, _ in holidays_list]
+
 
 def test_count_working_days_no_holidays():
     start_date = datetime.date(2024, 1, 1)
@@ -18,11 +20,13 @@ def test_count_working_days_no_holidays():
     result = count_working_days(start_date, end_date, holidays_list)
     assert result == 5
 
+
 def test_count_working_days_with_holidays(holidays):
     start_date = datetime.date(2024, 1, 1)
     end_date = datetime.date(2024, 1, 10)
     result = count_working_days(start_date, end_date, holidays)
     assert result == 7  # Updated to reflect the adjustment in working_days_tr.py
+
 
 # Configure Loguru logger
 logger.add("tests.log", rotation="500 MB", level="INFO")
